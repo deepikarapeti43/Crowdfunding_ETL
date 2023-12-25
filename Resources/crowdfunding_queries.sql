802521ffc9a9655ecb2f8d4f9dc817ffd1394a24
deepikarapeti43 <127686390+deepikarapeti43@users.noreply.github.com>
2023-12-25 16:20:40 -0600

    Add sql schema and queries

---
+++ Resources/crowdfunding_queries.sql
@@ -0,0 +1,12 @@
+-- Retrieves 5 rows from campaign table
+select * from campaign Limit 5;
+
+-- Retrieves 5 rows from category table
+select * from category Limit 5;
+
+-- Retrieves 5 rows from subcategory table
+select * from subcategory Limit 5;
+
+-- Retrieves 5 rows from contacts table
+select * from contacts Limit 5;
+
